a793059e2aab5f2a4455b8d0b2248ad5504ce2c8
adam <ibrahimadam193@gmail.com>
2018-03-13 20:58:48 -0400

    Put Paillier functions into a single class
    Changes to be committed:
          new file:   paillier.cpp
          new file:   paillier.h

---
+++ paillier.cpp
@@ -0,0 +1,62 @@
+#include "paillier.h"
+
+NTL::ZZ generateCoprimeNumber(NTL::ZZ n) {
+    NTL::ZZ ret;
+    while (true) {
+        ret = RandomBnd(n);
+        if (NTL::GCD(ret, n) == 1) { return ret; }
+    }
+}
+
+Paillier::Paillier() {
+    /* Length in bits. */
+    long keyLength = 512;
+    NTL::ZZ p, q;
+    GenPrimePair(p, q, keyLength);
+    modulus = p * q;
+    generator = modulus + 1;
+    NTL::ZZ phi = (p - 1) * (q - 1);
+    // LCM(p, q) = p * q / GCD(p, q);
+    lambda = phi / NTL::GCD(p - 1, q - 1);
+    lambdaInverse = NTL::InvMod(lambda, modulus);
+}
+
+Paillier::Paillier(NTL::ZZ modulus, NTL::ZZ lambda) {
+    this->modulus = modulus;
+    generator = this->modulus + 1;
+    this->lambda = lambda;
+    lambdaInverse = NTL::InvMod(this->lambda, this->modulus);
+}
+
+void Paillier::GenPrimePair(NTL::ZZ& p, NTL::ZZ& q,
+                               long keyLength) {
+    while (true) {
+        long err = 80;
+        p = NTL::GenPrime_ZZ(keyLength/2, err); 
+        NTL::ZZ q = NTL::GenPrime_ZZ(keyLength/2, err);
+        while (p != q) {
+            q = NTL::GenPrime_ZZ(keyLength/2, err);
+        }
+        NTL::ZZ n = p * q;
+        NTL::ZZ phi = (p - 1) * (q - 1);
+        if (NTL::GCD(n, phi) == 1) return;
+    }
+}
+
+NTL::ZZ Paillier::encrypt(NTL::ZZ message) {
+    NTL::ZZ random = generateCoprimeNumber(modulus);
+    NTL::ZZ ciphertext = 
+        NTL::PowerMod(generator, message, modulus * modulus) *
+        NTL::PowerMod(random, modulus, modulus * modulus);
+    return ciphertext % (modulus * modulus);
+           
+}
+NTL::ZZ Paillier::decrypt(NTL::ZZ ciphertext) {
+    /* NOTE: NTL::PowerMod will fail if the first input is too large
+     * (which I assume means larger than modulus).
+     */
+    NTL::ZZ deMasked = NTL::PowerMod(ciphertext, lambda, 
+                                     modulus * modulus);
+    NTL::ZZ power = L_function(deMasked);
+    return (power * lambdaInverse) % modulus;
+}
---
+++ paillier.h
@@ -0,0 +1,62 @@
+#include <NTL/ZZ.h>
+#include <NTL/RR.h>
+#include <NTL/ZZ_pXFactoring.h>
+
+class Paillier {
+    public:
+    /* Completely generate everything, from scratch */
+    Paillier();
+    Paillier(NTL::ZZ modulus, NTL::ZZ lambda); 
+    //Paillier(path to public key, path to private key).
+
+    /* Paillier encryption function. Takes in a message from the
+     * integers modulo n (Paillier.modulus) and returns a message in
+     * the integers modulo n**2.
+     *
+     * Parameters
+     * ==========
+     * NTL::ZZ message : The message to encrypt, as a number.
+     *
+     * Returns
+     * =======
+     * NTL:ZZ ciphertext : The encyrpted message.
+     */
+    NTL::ZZ encrypt(NTL::ZZ message); 
+
+    /* Paillier decryption function. Takes in a cipertext from Z mod
+     * n**2 and returns a message in the Z mod n.
+     *
+     * Parameters
+     * ==========
+     * NTL::ZZ cipertext : The encrypted message.
+     *
+     * Returns
+     * =======
+     * NTL::ZZ message : The original message.
+     */
+    NTL::ZZ decrypt(NTL::ZZ ciphertext); 
+
+    private:
+    /* modulus = pq, where p and q are primes */
+    NTL::ZZ modulus;
+    NTL::ZZ generator;
+    NTL::ZZ lambda;
+    NTL::ZZ lambdaInverse;
+
+    /* The L function in the paillier cryptosystem.  See
+     * <https://en.wikipedia.org/wiki/Paillier_cryptosystem> for more
+     * details.
+     *
+     * Parameters
+     * ==========
+     * NTL::ZZ x : The argument to L.
+     * NTL::ZZ n : The paillier modulus.
+     *
+     * Returns
+     * =======
+     * NTL::ZZ result : (x - 1) / n
+     */
+    NTL::ZZ L_function(NTL::ZZ n) { return (n - 1) / modulus; }
+
+    void GenPrimePair(NTL::ZZ& p, NTL::ZZ& q, long keyLength); 
+};
